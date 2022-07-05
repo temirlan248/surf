31134b5ee8fa3a0c8a3384543f802f219d0a9ff0
temir248 <temirlan.muslimov.1@gmail.com>
2022-07-05 09:54:52 +0600

    add splash screen

---
+++ app/src/main/java/kz/temir/gallery/SplashActivity.kt
@@ -0,0 +1,19 @@
+package kz.temir.gallery
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3_000)
+    }
+}
